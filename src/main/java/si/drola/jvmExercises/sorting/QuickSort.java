029b9637299519962f4bbbece8a3aeb690f56446
Matjaž Drolc <mdrolc@gmail.com>
2016-09-02 17:24:15 +0200

    QS, WIP

---
+++ src/main/java/si/drola/jvmExercises/sorting/QuickSort.java
@@ -0,0 +1,15 @@
+package si.drola.jvmExercises.sorting;
+
+public class QuickSort implements ISorter {
+    public int[] sort(int[] numbers) {
+        return new int[0];
+    }
+
+    protected void quicksort(int[] A, int p, int r) {
+
+    }
+
+    protected int partition(int[] A, int p, int r) {
+        return 0;
+    }
+}
