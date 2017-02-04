231fe69c3c2cb7a9af28be323c4b925fe7e474ec
Matjaž Drolc <mdrolc@gmail.com>
2017-02-04 23:20:52 +0100

    Refactored to facilitate existing BST code in RB tree

--- src/main/java/si/drola/jvmExercises/dataStructures/RedBlackTree.java
+++ src/main/java/si/drola/jvmExercises/dataStructures/RedBlackTree.java
@@ -1,110 +1,31 @@
 package si.drola.jvmExercises.dataStructures;
 
+import java.util.function.Consumer;
 
-public class RedBlackTree {
-    public RedBlackNode root;
-
-    public void leftRotate(RedBlackNode x) {
-        RedBlackNode y = x.right;
-        x.right = y.left;
-        if(y.left != null) {
-            y.left.parent = x;
-        }
-        y.parent = x.parent;
-        if(x.parent == null) {
-            this.root = y;
-        } else if(x == x.parent.left) {
-            x.parent.left = y;
-        }
-        y.left = x;
-        x.parent = y;
+public class RedBlackTree extends BinarySearchTree {
+    public enum Color {
+        RED,
+        BLACK
     }
 
-    public void rightRotate(RedBlackNode x) {
-        RedBlackNode y = x.left;
-        x.left = y.right;
-        if(y.right != null) {
-            y.right.parent = x;
+    public static class Node extends BinarySearchTree.Node {
+        Color color = Color.BLACK;
+
+        public Node(int val, Node left, Node right) {
+            super(val, left, right);
         }
-        y.parent = x.parent;
-        if(x.parent == null) {
-            this.root = y;
-        } else if(x == x.parent.right) {
-            x.parent.right = y;
+
+        public Node(int val) {
+            super(val);
         }
-        y.right = x;
-        x.parent = y;
-    }
 
-    public void insert(RedBlackNode z) {
-        RedBlackNode y = null;
-        RedBlackNode x = this.root;
-        while(x != null) {
-            y = x;
-            if(z.value < x.value) {
-                x = x.left;
-            } else {
-                x = x.right;
-            }
+        public Node() {
+            super();
         }
-        z.parent = y;
-        if(y == null) {
-            this.root = z;
-        } else if(z.value < y.value) {
-            y.left = z;
-        } else {
-            y.right = z;
-        }
-        z.left = null;
-        z.right = null;
-        z.color = RedBlackNode.Color.RED;
 
-        insertFixup(z);
-    }
-
-    private void insertFixup(RedBlackNode z) {
-        RedBlackNode y;
-
-        while (z.parent.color == RedBlackNode.Color.RED) {
-            if (z.parent == z.parent.parent.left) {
-                y = z.parent.parent.right;
-                if(y.color == RedBlackNode.Color.RED) {
-                    z.parent.color = RedBlackNode.Color.BLACK;
-                    y.color = RedBlackNode.Color.BLACK;
-                    z.parent.parent.color = RedBlackNode.Color.RED;
-                    z = z.parent.parent;
-                } else {
-                    if (z == z.parent.right) {
-                        z = z.parent;
-                        leftRotate(z);
-                    }
-
-                    z.parent.color = RedBlackNode.Color.BLACK;
-                    z.parent.parent.color = RedBlackNode.Color.RED;
-                    rightRotate(z.parent.parent);
-                }
-            } else {
-                y = z.parent.parent.left;
-                if(y.color == RedBlackNode.Color.RED) {
-                    z.parent.color = RedBlackNode.Color.BLACK;
-                    y.color = RedBlackNode.Color.BLACK;
-                    z.parent.parent.color = RedBlackNode.Color.RED;
-                    z = z.parent.parent;
-                } else {
-                    if( z == z.parent.left) {
-                        z = z.parent;
-                        rightRotate(z);
-                    }
-
-                    z.parent.color = RedBlackNode.Color.BLACK;
-                    z.parent.parent.color = RedBlackNode.Color.RED;
-                    leftRotate(z.parent.parent);
-                }
-            }
-
-            this.root.color = RedBlackNode.Color.BLACK;
+        @Override
+        public String toString() {
+            return String.format("(%s, %d)", color == Color.RED ? "red" : "black", val);
         }
     }
-
-
 }
---
+++ src/test/java/si/drola/jvmExercises/dataStructures/RedBlackTreePropertiesTest.java
@@ -0,0 +1,50 @@
+package si.drola.jvmExercises.dataStructures;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnitQuickcheck.class)
+public class RedBlackTreePropertiesTest {
+    @Property
+    public void validateCreation(int[] nodeValues) {
+        RedBlackTree bst = new RedBlackTree();
+        for (int i = 0; i < nodeValues.length; i++) {
+            bst.insert(new RedBlackTree.Node(nodeValues[i]));
+        }
+
+        Arrays.sort(nodeValues);
+
+        BinarySearchTreePropertiesTest.assertValidBst(bst);
+        BinarySearchTreePropertiesTest.assertBstWithValues(nodeValues, bst);
+    }
+
+    @Property
+    public void validateDeletion(int[] nodeValues) {
+        if(nodeValues.length == 0) {
+            assertTrue(true);
+            return;
+        }
+
+
+        RedBlackTree bst = new RedBlackTree();
+        RedBlackTree.Node last = null;
+        for (int i = 0; i < nodeValues.length; i++) {
+            last = new RedBlackTree.Node(nodeValues[i]);
+            bst.insert(last);
+        }
+
+        bst.delete(last);
+
+        nodeValues = Arrays.copyOfRange(nodeValues, 0, nodeValues.length - 1);
+        Arrays.sort(nodeValues);
+
+        BinarySearchTreePropertiesTest.assertValidBst(bst);
+        BinarySearchTreePropertiesTest.assertBstWithValues(nodeValues, bst);
+    }
+}
+
